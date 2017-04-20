bc9a68b26475e0d559bd78852fabe91a35091415
wendrey <lordwendrey@gmail.com>
2017-04-20 17:10:14 -0300

    NodeVisitor

---
+++ nodes/NodeVisitor.py
@@ -0,0 +1,28 @@
+class NodeVisitor(object) :
+
+    def visit(self,node):
+        """
+        Execute a method of the form visit_NodeName(node) where
+        NodeName is the name of the class of a particular node.
+        """
+        if node:
+            method = ’visit_’ + node.__class__.__name__
+            visitor = getattr(self, method, self.generic_visit)
+            return visitor(node)
+        else:
+            return None
+            
+     def generic_visit(self,node):
+        """
+        Method executed if no applicable visit_ method can be found.
+        This examines the node to see if it has _fields, is a list,
+        or can be further traversed.
+        """
+        for field in getattr(node,"_fields"):
+            value = getattr(node,field,None)
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item,AST):
+                        self.visit(item)
+            elif isinstance(value, AST):
+                self.visit(value)
